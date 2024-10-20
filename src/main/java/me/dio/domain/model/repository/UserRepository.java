dc9836c022d3e2704357a73c881ae1d1756f0ecb
srmatheusmaciel <srmatheusmaciel@hotmail.com>
2024-10-20 10:48:13 -0300

    Implementação da camada de dominio
    Signed-off-by: srmatheusmaciel <srmatheusmaciel@hotmail.com>

---
+++ src/main/java/me/dio/domain/model/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package me.dio.domain.model.repository;
+
+import me.dio.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+}
